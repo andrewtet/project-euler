c526bdcaa7d7e619ec5da317f489b511097f42c5
atetnowski <atetnowski@gmail.com>
2015-06-19 20:51:06 -0700

    finished problem 8

--- problem0008.js
+++ problem0008.js
@@ -1,101 +1,64 @@
 /*
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+
+Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 */
 
-function App(largest, smallest){
+// Normally I would enter this into the class, but for readability I'm putting it in a variable up here
+var numString = '7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450';
+
+
+function App(numDigits, string){
 	var self = this;
-	self.smallestMultiple = 0;
-	self.factors = {};
-	self.maxFactors = {};
-	self.largestNumber = largest;
-	self.smallestNumber = smallest;
+	self.searchString = string;
+	self.numDigits = numDigits;
+	self.product = 0;
 
 	self.init = function(){
 		self.instructions();
-		self.getFactors();
-		self.getMaxFactors();
-		self.getMultiple();
-		self.printMultiple();
+		self.getProduct();
+		self.printProduct();
 	};
 
 	self.instructions = function(){
-		console.log('2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.');
-		console.log('What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?');
+		console.log('The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.');
+		console.log(self.searchString);
+		console.log('Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?');
 	};
 
-	self.getFactors = function(){
-		for(var i = self.smallestNumber; i <= self.largestNumber; i++){
-			var arr = self.factorNumber(i);
-			self.factors[i] = self.getFactorCount(arr);
-		}
-	}
+	self.getProduct = function(){
+		for(var i = 0; i <= self.searchString.length - self.numDigits; i++){
+			
+			var split = self.searchString.substr(i, self.numDigits);
 
-	self.getMaxFactors = function(){
-		for(f in self.factors){
-			var thisFactor = self.factors[f];
-			for(n in thisFactor){
-				if(!self.maxFactors[n] ||
-					self.maxFactors[n] < thisFactor[n]){
-					self.maxFactors[n] = thisFactor[n];
-				}
+			if(split.indexOf('0') > -1){
+				// if string contains 0, product will be 0
+				// no need to calculate it
+				continue;
+			}
+
+			var prod = 1;
+			var splitArr = split.split('');
+			for(var j = 0; j < splitArr.length; j++){
+				prod *= Number(splitArr[j]);
+			}
+
+			if(prod > self.product){
+				self.product = prod;
 			}
 		}
 	}
 
-	self.factorNumber = function(num){
-		
-		if(num === 1 || num === 2){
-			return [num];
-		}
-
-		var workingNumber = num;
-		var factors = [];
-
-		while(workingNumber > 1){
-			for(var i = 2; i <= workingNumber; i++){
-				if(workingNumber % i === 0){
-					workingNumber = workingNumber / i;
-					factors.push(i);
-					break;
-				}
-			}
-		}
-
-		return factors;
-	};
-
-	self.getFactorCount = function(arr){
-		var counts = {};
-
-		for(var i = 0; i < arr.length; i++){
-			var num = arr[i];
-			counts[num] = counts[num] ? counts[num]+1 : 1;
-		}
-
-		return counts;
-	}
-
-	self.getMultiple = function(){
-		var multiple = 1;
-
-		for(n in self.maxFactors){
-			console.log(self.maxFactors[n]);
-			multiple *= Math.pow(n, self.maxFactors[n]);
-		}
-
-		self.smallestMultiple = multiple;
-	}
-
-	self.printMultiple = function(){
-		console.log(self.smallestMultiple);
+	self.printProduct = function(){
+		console.log(self.product);
 	}
 };
 
 //Test
-//var t = new App(10,1);
+//var t = new App(4, numString);
 //t.init();
 
-var a = new App(20,1);
+var a = new App(13, numString);
 a.init();
