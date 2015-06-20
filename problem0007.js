ee89641aad9ffa2caad720f09aa45b3cda233a0c
atetnowski <atetnowski@gmail.com>
2015-06-19 20:28:20 -0700

    finished problem 7

--- problem0007.js
+++ problem0007.js
@@ -10,63 +10,62 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 */
 
-function App(largest, smallest){
+function App(primeToFind){
 	var self = this;
-	self.difference = 0;
-	self.sumOfSquares = 0;
-	self.squareOfSums = 0;
-	self.largestNumber = largest;
-	self.smallestNumber = smallest;
+	self.prime = 0;
+	self.primeToFind = primeToFind;
+	self.primesFound = 0;
 
 	self.init = function(){
 		self.instructions();
-		self.getSquareOfSums();
-		self.getSumOfSquares();
-		self.getDifference();
-		self.printDifference();
+		self.getPrime();
+		self.printPrime();
 	};
 
 	self.instructions = function(){
-		console.log('The sum of the squares of the first ten natural numbers is,');
-		console.log('12 + 22 + ... + 102 = 385');
-		console.log('The square of the sum of the first ten natural numbers is,');
-		console.log('(1 + 2 + ... + 10)2 = 552 = 3025');
-		console.log('Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.');
-		console.log('Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.');
+		console.log('By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.');
+		console.log('What is the 10 001st prime number?');
 	};
 
-	self.getSquareOfSums = function(){
-		var sum = 0;
+	self.getPrime = function(){
+		self.primesFound = 2; // Accounting for known primes 1 and 2 allowing us to only iterate over idd numbers
 
-		for(var i = self.smallestNumber; i <= self.largestNumber; i++){
-			sum += Number(i);
+		var count = 3; // number to check if it is prime
+
+		while(self.primesFound < self.primeToFind){
+			count += 2; // Even numbers cant be prime so they arent checked
+
+			if(self.isPrime(count)){
+				self.primesFound++;
+			}
+
 		}
 
-		self.squareOfSums = Math.pow(sum,2);
+		self.prime = count;
 	};
 
-	self.getSumOfSquares = function(){
-		var sum = 0;
+	self.isPrime = function(num){
+		var isPrime = true;
+		var halfNum = Math.ceil(num / 2); // No number is divisable by over half
 
-		for(var i = self.smallestNumber; i <= self.largestNumber; i++){
-			sum += Math.pow(i,2);
+		for(var i = 2; i < halfNum; i++){
+			if(num % i == 0){
+				isPrime = false;
+				break;
+			}
 		}
 
-		self.sumOfSquares = sum;
-	};
+		return isPrime;
+	}
 
-	self.getDifference = function(){
-		self.difference = self.squareOfSums - self.sumOfSquares;
-	};
-
-	self.printDifference = function(){
-		console.log(self.difference);
+	self.printPrime = function(){
+		console.log(self.prime);
 	};
 };
 
 //Test
-//var t = new App(10,1);
-//t.init(); // Should answer 2640
+var t = new App(6);
+t.init(); // Should answer 13
 
-var a = new App(100,1);
+var a = new App(10001);
 a.init();
